d3879dc6594acd8d663c88703bb5762d7df83aa4
hater4u <ndrozdov123@gmail.com>
2020-07-13 23:40:44 +0700

    refactor views.py, add view for delete

--- experimentsbase/views.py
+++ experimentsbase/views.py
@@ -52,35 +52,35 @@ def check_auth_user(request, args):
 def get_taxon_path(taxon_id):
     hierarchy = list()
     try:
-        values = requests.get(API_URL + "/taxa", params={"id": taxon_id}).json()
-        hierarchy.append({"id": taxon_id, "name": values["value"]["name"]})
-        parent_id = values["value"]["parentId"]
+        values = requests.get(API_URL + '/taxa', params={'id': taxon_id}).json()
+        hierarchy.append({'id': taxon_id, 'name': values['value']['name']})
+        parent_id = values['value']['parentId']
         while parent_id:
-            values = requests.get(API_URL + "/taxa", params={"id": parent_id}).json()
-            parent_name = values["value"]["name"]
-            hierarchy.append({"id": parent_id, "name": parent_name})
-            parent_id = values["value"]["parentId"]
+            values = requests.get(API_URL + '/taxa', params={'id': parent_id}).json()
+            parent_name = values['value']['name']
+            hierarchy.append({'id': parent_id, 'name': parent_name})
+            parent_id = values['value']['parentId']
         return hierarchy
     except Exception:
-        print("hierarchy found error")
+        print('hierarchy found error')
         return []
 
 
 def get_taxon_children(taxon_id):
     try:
-        return requests.get(API_URL + "/taxa/byParent", params={"parentId": taxon_id}).json()["value"]
+        return requests.get(API_URL + '/taxa/byParent', params={'parentId': taxon_id}).json()['value']
     except Exception:
-        print("children found error")
+        print('children found error')
         return []
 
 
 def experiments_search(search_dict):
     try:
         headers = {'Content-type': 'application/json'}
-        return requests.post(API_URL + "/experiments/search",
-                             data=json.dumps(search_dict), headers=headers).json()["value"]
+        return requests.post(API_URL + '/experiments/search',
+                             data=json.dumps(search_dict), headers=headers).json()['value']
     except Exception as e:
-        print("experiments found error: " + str(e))
+        print('experiments found error: ' + str(e))
         return []
 
 
@@ -172,41 +172,41 @@ def parse_experiments_data(request):
 
 
 def translate_experiments(experiments_list):
-    way_of_life = {"DIURNAL": "Дневное", "NOCTURNAL": "Ночное", "TWILIGHT": "Сумеречное", "OTHER": "Другое"}
-    habitat = {"WILD": "Дикое", "LABORATORY": "Лабораторное", "FARM": "Фермерское", "OTHER": "Другое"}
-    gender = {"MALE": "Мужской", "FEMALE": "Женский", "OTHER": "Другое"}
+    way_of_life = {'DIURNAL': 'Дневное', 'NOCTURNAL': 'Ночное', 'TWILIGHT': 'Сумеречное', 'OTHER': 'Другое'}
+    habitat = {'WILD': 'Дикое', 'LABORATORY': 'Лабораторное', 'FARM': 'Фермерское', 'OTHER': 'Другое'}
+    gender = {'MALE': 'Мужской', 'FEMALE': 'Женский', 'OTHER': 'Другое'}
 
     for el in experiments_list:
-        el["wayOfLife"] = way_of_life[el["wayOfLife"]]
-        el["habitat"] = habitat[el["habitat"]]
-        el["gender"] = gender[el["gender"]]
+        el['wayOfLife'] = way_of_life[el['wayOfLife']]
+        el['habitat'] = habitat[el['habitat']]
+        el['gender'] = gender[el['gender']]
 
     return experiments_list
 
 
 def taxons_id(request, taxon_id):
     args = dict()
-    args["taxon_id"] = taxon_id
+    args['taxon_id'] = taxon_id
     hierarchy = get_taxon_path(taxon_id)
     hierarchy.reverse()
-    args["hierarchy"] = hierarchy
-    args["children"] = get_taxon_children(taxon_id)
+    args['hierarchy'] = hierarchy
+    args['children'] = get_taxon_children(taxon_id)
 
-    if taxon_id == "":
+    if taxon_id == '':
         experiments_dict = []
     else:
-        search_dict = {"taxonIds": [taxon_id]}
+        search_dict = {'taxonIds': [taxon_id]}
         experiments_dict = experiments_search(search_dict)
 
-    args["experiments"] = translate_experiments(experiments_dict)
+    args['experiments'] = translate_experiments(experiments_dict)
 
     args = check_auth_user(request, args)
 
-    return render(request, "taxons.html", args)
+    return render(request, 'taxons.html', args)
 
 
 def taxons(request):
-    return taxons_id(request, "")
+    return taxons_id(request, '')
 
 
 def taxon_parent_search(request):
@@ -290,15 +290,15 @@ def taxon_rename_id(request, taxon_id):
     if request.user.is_authenticated:
         if request.user.is_staff:
             if request.POST:
-                if request.POST["taxonName"] == "":
+                if request.POST['taxonName'] == '':
                     args['success'] = False
                     args['message'] = 'Пустое имя таксона'
                     return render(request, 'taxon/rename.html', args)
 
                 try:
                     headers = {'Content-Type': 'application/json'}
-                    if requests.post(API_URL + "/taxa/rename",
-                                     data=json.dumps({"id": taxon_id, "name": request.POST['taxonName']}),
+                    if requests.post(API_URL + '/taxa/rename',
+                                     data=json.dumps({'id': taxon_id, 'name': request.POST['taxonName']}),
                                      headers=headers,
                                      auth=('docker_admin', '123qweasdzxc')).status_code == 200:
                         args['success'] = True
@@ -331,13 +331,13 @@ def taxon_move_id(request, taxon_id):
     args.update(csrf(request))
     if request.user.is_authenticated:
         if request.user.is_staff:
-            args["taxon_id"] = taxon_id
+            args['taxon_id'] = taxon_id
             try:
                 values = requests.get(API_URL + '/taxa',
                                       params={'id': taxon_id}).json()['value']
                 args['taxon_name'] = values['name']
                 if values['parentId']:
-                    args["taxon_parent_name"] = requests.get(API_URL + '/taxa',
+                    args['taxon_parent_name'] = requests.get(API_URL + '/taxa',
                                                          params={'id': values['parentId']}).json()['value']['name']
                 else:
                     args['taxon_parent_name'] = ''
@@ -402,10 +402,10 @@ def taxon_delete_id(request, taxon_id):
     args.update(csrf(request))
     if request.user.is_authenticated:
         if request.user.is_staff:
-            args["taxon_id"] = taxon_id
+            args['taxon_id'] = taxon_id
 
             try:
-                args["taxon_name"] = requests.get(API_URL + '/taxa', params={'id': taxon_id}).json()['value'][
+                args['taxon_name'] = requests.get(API_URL + '/taxa', params={'id': taxon_id}).json()['value'][
                     'name']
             except Exception as e:
                 print('Error delete taxon, getting taxon_name :' + str(e))
@@ -416,7 +416,7 @@ def taxon_delete_id(request, taxon_id):
             if request.POST:
                 if request.POST['delete']:
                     try:
-                        if requests.post(API_URL + "/taxa/delete", params={"id": taxon_id},
+                        if requests.post(API_URL + '/taxa/delete', params={'id': taxon_id},
                                          auth=('docker_admin', '123qweasdzxc')).status_code == 200:
                             args['success'] = True
                             args['message'] = 'Таксон успешно удалён'
@@ -483,21 +483,32 @@ def experiment_delete(request, experiment_id):
     args.update(csrf(request))
     if request.user.is_authenticated:
         if request.user.is_staff:
+            args['experiment_id'] = experiment_id
+            try:
+                args['experiment_name'] = requests.get(API_URL + '/experiments', params={'id': experiment_id}).json()[
+                    'value']['name']
+            except Exception as e:
+                print('Error delete experiment, getting experiment_name :' + str(e))
+                args['success'] = False
+                args['message'] = 'Проблема доступа API. Обратитесь к администратору сервера.'
+                return render(request, 'experiment/delete.html', args)
             if request.POST:
                 try:
-                    if requests.post(API_URL + "/experiments/delete", params={"id": experiment_id},
+                    if requests.post(API_URL + '/experiments/delete', params={'id': experiment_id},
                                      auth=('docker_admin', '123qweasdzxc')).status_code == 200:
                         args['success'] = True
                         args['message'] = 'Эксперимент успешно удалён'
                     else:
                         args['success'] = False
                         args['message'] = 'Эксперимент не был удалён. Обратитесь к администратору сервера.'
-                    return render(request, 'taxon/delete.html', args)
+                    return render(request, 'experiment/delete.html', args)
                 except Exception as e:
                     print('error delete experiment:' + str(e))
-                    return render(request, 'experiment/', args)
+                    args['success'] = False
+                    args['message'] = 'Проблема доступа API. Обратитесь к администратору сервера.'
+                    return render(request, 'experiment/delete.html', args)
             else:
-                return render(request, 'experiment/', args)
+                return render(request, 'experiment/delete.html', args)
         else:
             return render(request, 'reg/403.html')
     else:
@@ -511,7 +522,7 @@ def get_torrent(request):
     if request.user.is_authenticated:
         if request.user.is_staff:
             if request.POST:
-                "123"
+                '123'
             else:
                 return render(request, 'experiment/', args)
         else:
@@ -526,141 +537,141 @@ def experiments(request):
     if request.POST:
         args.update(csrf(request))
         search_dict = dict()
-        if not request.POST["experimentName"] == "":
-            search_dict["name"] = request.POST["experimentName"]
+        if not request.POST['experimentName'] == '':
+            search_dict['name'] = request.POST['experimentName']
 
-        if not request.POST["taxonId"] == "":
-            search_dict["taxonIds"] = [request.POST["taxonId"]]
+        if not request.POST['taxonId'] == '':
+            search_dict['taxonIds'] = [request.POST['taxonId']]
 
         # Ways of life
-        search_dict["waysOfLife"] = []
-        if request.POST.get("diurnalWayCheckbox", False):
-            search_dict["waysOfLife"].append("DIURNAL")
+        search_dict['waysOfLife'] = []
+        if request.POST.get('diurnalWayCheckbox', False):
+            search_dict['waysOfLife'].append('DIURNAL')
 
-        if request.POST.get("nocturnalWayCheckbox", False):
-            search_dict["waysOfLife"].append("NOCTURNAL")
+        if request.POST.get('nocturnalWayCheckbox', False):
+            search_dict['waysOfLife'].append('NOCTURNAL')
 
-        if request.POST.get("twilightWayCheckbox", False):
-            search_dict["waysOfLife"].append("TWILIGHT")
+        if request.POST.get('twilightWayCheckbox', False):
+            search_dict['waysOfLife'].append('TWILIGHT')
 
-        if request.POST.get("otherWayCheckbox", False):
-            search_dict["waysOfLife"].append("OTHER")
+        if request.POST.get('otherWayCheckbox', False):
+            search_dict['waysOfLife'].append('OTHER')
 
         # Habitat
-        search_dict["habitats"] = []
-        if request.POST.get("wildHabitatCheckbox", False):
-            search_dict["habitats"].append("WILD")
+        search_dict['habitats'] = []
+        if request.POST.get('wildHabitatCheckbox', False):
+            search_dict['habitats'].append('WILD')
 
-        if request.POST.get("laboratoryHabitatCheckbox", False):
-            search_dict["habitats"].append("LABORATORY")
+        if request.POST.get('laboratoryHabitatCheckbox', False):
+            search_dict['habitats'].append('LABORATORY')
 
-        if request.POST.get("farmHabitatCheckbox", False):
-            search_dict["habitats"].append("FARM")
+        if request.POST.get('farmHabitatCheckbox', False):
+            search_dict['habitats'].append('FARM')
 
-        if request.POST.get("otherHabitatCheckbox", False):
-            search_dict["habitats"].append("OTHER")
+        if request.POST.get('otherHabitatCheckbox', False):
+            search_dict['habitats'].append('OTHER')
 
         # Gender
-        # search_dict["gender"] = []
-        # if request.POST["maleGenderCheckbox"]:
-        #     search_dict["gender"].append("MALE")
+        # search_dict['gender'] = []
+        # if request.POST['maleGenderCheckbox']:
+        #     search_dict['gender'].append('MALE')
         #
-        # if request.POST["femaleGenderCheckbox"]:
-        #     search_dict["gender"].append("FEMALE")
+        # if request.POST['femaleGenderCheckbox']:
+        #     search_dict['gender'].append('FEMALE')
         #
-        # if request.POST["otherGenderCheckbox"]:
-        #     search_dict["gender"].append("OTHER")
+        # if request.POST['otherGenderCheckbox']:
+        #     search_dict['gender'].append('OTHER')
 
         # Age
-        if request.POST["ageFrom"] == "":
-            search_dict["ageFrom"] = "null"
+        if request.POST['ageFrom'] == '':
+            search_dict['ageFrom'] = 'null'
         else:
-            search_dict["ageFrom"] = request.POST["ageFrom"]
+            search_dict['ageFrom'] = request.POST['ageFrom']
 
-        if request.POST["ageTo"] == "":
-            search_dict["ageTo"] = "null"
+        if request.POST['ageTo'] == '':
+            search_dict['ageTo'] = 'null'
         else:
-            search_dict["ageTo"] = request.POST["ageTo"]
+            search_dict['ageTo'] = request.POST['ageTo']
 
         # Weight
-        if request.POST["weightFrom"] == "":
-            search_dict["weightFrom"] = "null"
+        if request.POST['weightFrom'] == '':
+            search_dict['weightFrom'] = 'null'
         else:
-            search_dict["weightFrom"] = request.POST["weightFrom"]
+            search_dict['weightFrom'] = request.POST['weightFrom']
 
-        if request.POST["weightTo"] == "":
-            search_dict["weightTo"] = "null"
+        if request.POST['weightTo'] == '':
+            search_dict['weightTo'] = 'null'
         else:
-            search_dict["weightTo"] = request.POST["weightTo"]
+            search_dict['weightTo'] = request.POST['weightTo']
 
         # Length
-        if request.POST["lengthFrom"] == "":
-            search_dict["lengthFrom"] = "null"
+        if request.POST['lengthFrom'] == '':
+            search_dict['lengthFrom'] = 'null'
         else:
-            search_dict["lengthFrom"] = request.POST["lengthFrom"]
+            search_dict['lengthFrom'] = request.POST['lengthFrom']
 
-        if request.POST["lengthTo"] == "":
-            search_dict["lengthTo"] = "null"
+        if request.POST['lengthTo'] == '':
+            search_dict['lengthTo'] = 'null'
         else:
-            search_dict["lengthTo"] = request.POST["lengthTo"]
+            search_dict['lengthTo'] = request.POST['lengthTo']
 
         # Environmental factors
-        if not request.POST["environmentalFactors"] == "":
-            search_dict["environmentalFactors"] = [request.POST["environmentalFactors"]]
+        if not request.POST['environmentalFactors'] == '':
+            search_dict['environmentalFactors'] = [request.POST['environmentalFactors']]
 
         # Diseases
-        if not request.POST["diseases"] == "":
-            search_dict["diseases"] = [request.POST["diseases"]]
+        if not request.POST['diseases'] == '':
+            search_dict['diseases'] = [request.POST['diseases']]
 
         # Withdraw place
-        if not request.POST["withdrawPlace"] == "":
-            search_dict["withdrawPlace"] = request.POST["withdrawPlace"]
+        if not request.POST['withdrawPlace'] == '':
+            search_dict['withdrawPlace'] = request.POST['withdrawPlace']
 
         # Withdraw date
-        if request.POST["withdrawDateFrom"] != "":
-            #     search_dict["withdrawDateFrom"] = "null"
+        if request.POST['withdrawDateFrom'] != '':
+            #     search_dict['withdrawDateFrom'] = 'null'
             # else:
-            search_dict["withdrawDateFrom"] = request.POST["withdrawDateFrom"] + " 00:00:00"
+            search_dict['withdrawDateFrom'] = request.POST['withdrawDateFrom'] + ' 00:00:00'
 
-        if request.POST["withdrawDateTo"] != "":
-            #     search_dict["withdrawDateTo"] = "null"
+        if request.POST['withdrawDateTo'] != '':
+            #     search_dict['withdrawDateTo'] = 'null'
             # else:
-            search_dict["withdrawDateTo"] = request.POST["withdrawDateTo"] + " 00:00:00"
+            search_dict['withdrawDateTo'] = request.POST['withdrawDateTo'] + ' 00:00:00'
 
         # Seconds post mortem
-        if request.POST["secondsPostMortemFrom"] == "":
-            search_dict["secondsPostMortemFrom"] = "null"
+        if request.POST['secondsPostMortemFrom'] == '':
+            search_dict['secondsPostMortemFrom'] = 'null'
         else:
-            search_dict["secondsPostMortemFrom"] = request.POST["secondsPostMortemFrom"]
+            search_dict['secondsPostMortemFrom'] = request.POST['secondsPostMortemFrom']
 
-        if request.POST["secondsPostMortemTo"] == "":
-            search_dict["secondsPostMortemTo"] = "null"
+        if request.POST['secondsPostMortemTo'] == '':
+            search_dict['secondsPostMortemTo'] = 'null'
         else:
-            search_dict["secondsPostMortemTo"] = request.POST["secondsPostMortemTo"]
+            search_dict['secondsPostMortemTo'] = request.POST['secondsPostMortemTo']
 
         # Temperature
-        if request.POST["temperatureFrom"] == "":
-            search_dict["temperatureFrom"] = "null"
+        if request.POST['temperatureFrom'] == '':
+            search_dict['temperatureFrom'] = 'null'
         else:
-            search_dict["temperatureFrom"] = request.POST["temperatureFrom"]
+            search_dict['temperatureFrom'] = request.POST['temperatureFrom']
 
-        if request.POST["temperatureTo"] == "":
-            search_dict["temperatureTo"] = "null"
+        if request.POST['temperatureTo'] == '':
+            search_dict['temperatureTo'] = 'null'
         else:
-            search_dict["temperatureTo"] = request.POST["temperatureTo"]
+            search_dict['temperatureTo'] = request.POST['temperatureTo']
 
         # Comments
-        if not request.POST["comments"] == "":
-            search_dict["comments"] = [request.POST["comments"]]
+        if not request.POST['comments'] == '':
+            search_dict['comments'] = [request.POST['comments']]
 
         # Metabolite names
-        if not request.POST["metaboliteNames"] == "":
-            search_dict["metaboliteNames"] = [request.POST["metaboliteNames"]]
+        if not request.POST['metaboliteNames'] == '':
+            search_dict['metaboliteNames'] = [request.POST['metaboliteNames']]
 
         try:
-            args["experiments"] = translate_experiments(experiments_search(search_dict))
+            args['experiments'] = translate_experiments(experiments_search(search_dict))
         except Exception as e:
-            print("Search error:" + str(e))
+            print('Search error:' + str(e))
             args = {}
 
-    return render(request, "experiments.html", args)
+    return render(request, 'experiments.html', args)
